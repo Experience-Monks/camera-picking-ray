1aed76a5a0716a0ea471f6c617f9322958d517e8
mattdesl <dave.des@gmail.com>
2015-05-15 10:53:02 -0400

    naming the function

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var set = require('gl-vec3/set')
 var sub = require('gl-vec3/subtract')
 var normalize = require('gl-vec3/normalize')
 
-module.exports = function(origin, direction, point, viewport, invProjView) {
+module.exports = createPickRay
+function createPickRay(origin, direction, point, viewport, invProjView) {
   set(origin, point[0], point[1], 0)
   set(direction, point[0], point[1], 1)
   unproject(origin, origin, viewport, invProjView)
